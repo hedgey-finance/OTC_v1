1db9bf02a7359c5ffde0d729cbe78e2cf24d8e3d
Alex Michelsen <57596674+icemanparachute@users.noreply.github.com>
2022-01-09 14:17:23 -0500

    Update FuturesNFT.sol

--- contracts/FuturesNFT.sol
+++ contracts/FuturesNFT.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+
 interface IWETH {
     function deposit() external payable;
     function transfer(address to, uint value) external returns (bool);
