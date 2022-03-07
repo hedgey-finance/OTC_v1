64e3072be6c1f56e1e8f6f36d63588179bfbf5b2
Alex Michelsen <57596674+icemanparachute@users.noreply.github.com>
2022-03-07 18:25:07 -0500

    add in natspec

--- contracts/CeloHedgeyOTC.sol
+++ contracts/CeloHedgeyOTC.sol
@@ -6,35 +6,64 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 
+/// @dev this is the one contract call that the OTC needs to interact with the NFT contract
+/// @dev the renounce ownership function that is part of the Ownable Libarary, inherited by the NFT contract, is intentionally omitted from this interface
+/// @dev the updateBaseURI function is intentionall ommitted from this interface
 interface IFuturesNFT {
     function createFuture(address vester, uint _amount, address _asset, uint _expiry) external returns (uint);
 }
 
 
-interface IWETH {
-    function deposit() external payable;
-    function transfer(address to, uint value) external returns (bool);
-    function withdraw(uint) external;
-}
 
+/// @dev this interface is a critical addition that is not part of the standard ERC-20 specifications
+/// @dev this is required to do the calculation of the total price required, when pricing things in the payment currency
+/// @dev only the payment currency is required to have a decimals impelementation on the ERC20 contract, otherwise it will fail
 interface Decimals {
     function decimals() external view returns (uint256);
 }
 
 
+/**
+ * @title HedgeyOTC is an over the counter contract with time locking abilitiy
+ * @notice This contract allows for a seller to generate a unique over the counter deal, which can be private or public
+ * @notice The public deals allow anyone to participate and purchase tokens from the seller, whereas a private deal allows only a single whitelisted address to participate 
+ * @notice The Seller chooses whether or not the tokens being sold will be locked, as well as the price for the offer
+*/
 contract HedgeyOTC is ReentrancyGuard {
     using SafeERC20 for IERC20;
 
 
+    /// @dev d is a strict uint for indexing the OTC deals one at a time
     uint public d = 0;
+    /// @dev we use this address to store a single futures contract, which is our NFT ERC721 contract address, which we point to for the minting process
     address public futureContract;
 
     constructor(address _fc) {
         futureContract = _fc;
     }
 
+    /**
+     * @notice Deal is the struct that defines a single OTC offer, created by a seller
+     * @param  Deal struct contains the following parameter definitions:
+     * @param 1) seller: This is the creator and seller of the deal, needs to be an address payable if the payment currenct is ETH / WETH
+     * @param 2) token: This is the token that the seller deposits into the contract and which they are selling over the counter. The address defines this ERC20
+     * @param ... the token ERC20 contract is required to have a public call function decimals() that returns a uint. This is required to price the amount of tokens being purchase
+     * @param ... by the buyer - calculating exactly how much to deliver to the seller. 
+     * @param 3) paymentCurrency: This is also an ERC20 which the seller will get paid in, during the act of a buyer buying tokens, and paying the seller in the paymentCurrency
+     * @param 4) remainingAmount: This initially is the entire deposit the seller is selling, but as people purchase chunks of the deal, the remaining amount is decreased to 0
+     * @param 5) minimumPurchase: This is the minimum chunk size that a buyer can purchase, defined by the seller. this prevents security issues of
+     * @param ... buyers purchasing 1wei worth of the token which can cause a 0 payment amount, resulting in a conflict. 
+     * @param 6) price: The Price is the per token cost which buyers pay to the seller, denominated in the payment currency. This is not the total price of the deal
+     * @param ... the total price is calculated by the remainingAmount * price (then adjusting for the decimals of the payment currency)
+     * @param 7) maturity: this is the unix block time for up until this deal is valid. After the maturity no purchases can be made.
+     * @param 8) unlockDate: this is the unix block time which may be used to time lock tokens that are sold. If the unlock date is 0 or less than current block time
+     * @param ... at the time of purchase, the tokens are not locked but rather delivered directly to the buyer from the contract
+     * @param 9) open: boolean for security purposes to check if this deal is still open and can be purchsed. When the remainingAmount == 0 or it has been cancelled by the seller open == false and no purcahses can be made
+     * @param 10) buyer: this is a whitelist address for the buyer. It can either be the Zero address - which indicates that Anyone can purchase
+     * @param ... or it is a single address that only that owner of the address can participate in purchasing the tokens
+    */
     struct Deal {
-        address payable seller;
+        address seller;
         address token;
         address paymentCurrency;
         uint remainingAmount;
@@ -43,25 +72,41 @@ contract HedgeyOTC is ReentrancyGuard {
         uint maturity;
         uint unlockDate;
         bool open;
-        address payable buyer;
+        address buyer;
     }
 
+    /// @dev the Deals are all mapped via the indexer d to deals mapping
     mapping (uint => Deal) public deals;
 
 
-    receive() external payable {}
-
-    function transferPymt(address _token, address from, address payable to, uint _amt) internal {
+    /// @dev internal function that handles transfering payments from buyers to sellers 
+    function transferPymt(address _token, address from, address to, uint _amt) internal {
         SafeERC20.safeTransferFrom(IERC20(_token), from, to, _amt);         
     }
 
-
+    /// @dev internal funciton that handles withdrawing tokens that are up for sale to buyers
     function withdraw(address _token, address payable to, uint _total) internal {
         SafeERC20.safeTransfer(IERC20(_token), to, _total);
     }
 
-    
 
+    /** 
+     * @notice This function is what the seller uses to create a new OTC offering
+     * @dev this function will pull in tokens from the seller, create a new struct as Deal indexed by the current uint d
+     * @dev this function does not allow for taxed / deflationary tokens - as the amount that is pulled into the contract must match with what is being sent  
+     * @dev this function requires that the _token has a decimals() public function on its ERC20 contract to be called 
+     * @param _token address is the token that the seller is going to create the over the counter offering for
+     * @param _paymentCurrency is the address of the opposite ERC20 that the seller wants to get paid in when selling the token (use WETH for ETH)
+     * @param amount is the amount of tokens that you as the seller want to sell
+     * @param min is the minimum amount of tokens that a buyer can purchase from you. this should be less than or equal to the total amount
+     * @param _price is the price per token which you would like to get paid, denominated in the payment currency
+     * @param _maturity is how long you would like to allow buyers to purchase tokens from this deal, in unix block time. this needs to be beyond current time
+     * @param _unlockDate is used if you are requiring that tokens purchased by buyers are locked. If this is set to 0 or anything less than current time 
+     * @param ... any tokens purchased will not be locked but immediately delivered to the buyers. Otherwise the unlockDate will lock the tokens in the associated 
+     * @param ... futures NFT contract - which will hold the tokens in escrow until the unlockDate has passed - whereupon the owner of the NFT can redeem the tokens
+     * @param _buyer is a special option to make this a private deal - where only the buyer's address can participate and make the purchase. If this is set to the 
+     * @param ... Zero address - then it is publicly available and anyone can purchase tokens from this deal
+    */
     function create(
         address _token,
         address _paymentCurrency,
@@ -70,74 +115,117 @@ contract HedgeyOTC is ReentrancyGuard {
         uint _price,
         uint _maturity,
         uint _unlockDate,
-        address payable _buyer
+        address _buyer
     ) payable external {
-        require(_maturity > block.timestamp);
-        require(amount >= min, "min error");
-        require((min * _price) / (10 ** Decimals(_paymentCurrency).decimals()) > 0, "minimum too small");
+        require(_maturity > block.timestamp, "HEC01: Maturity before block timestamp");
+        require(amount >= min, "HEC02: Amount less than minium");
+        /// @dev this checks to make sure that if someone purchases the minimum amount, it is never equal to 0
+        /// @dev where someone could find a small enough minimum to purchase all of the tokens for free.
+        require((min * _price) / (10 ** Decimals(_token).decimals()) > 0, "HEC03: Minimum smaller than 0");
+        /// @dev we check the before balance of this address for security
         uint currentBalance = IERC20(_token).balanceOf(address(this));
-        //pull in tokens
-        require(IERC20(_token).balanceOf(msg.sender) >= amount);
+        /// @dev this function physically pulls the tokens into the contract for escrow
+        require(IERC20(_token).balanceOf(msg.sender) >= amount, "HECB: Insufficient Balance");
         SafeERC20.safeTransferFrom(IERC20(_token), msg.sender, address(this), amount);
+        /// @dev check the current balance now that the tokens should be in the contract address
+        /// @dev we need to ensure that the balance matches the amount input into the parameters - since that amount is recorded on the Deal struct
         uint postBalance = IERC20(_token).balanceOf(address(this));
         assert(postBalance - currentBalance == amount);
-        deals[d++] = Deal(payable(msg.sender), _token, _paymentCurrency, amount, min, _price, _maturity, _unlockDate, true, payable(_buyer));
+        /// @dev creates the Deal struct with all of the parameters for inputs - and set the bool 'open' to true so that this offer can now be purchased
+        deals[d++] = Deal(msg.sender, _token, _paymentCurrency, amount, min, _price, _maturity, _unlockDate, true, _buyer);
         emit NewDeal(d - 1, msg.sender, _token, _paymentCurrency, amount, min, _price, _maturity, _unlockDate, true, _buyer);
     }
 
-    
 
+    /** 
+     * @notice This function lets a seller cancel their existing deal anytime they would like to
+     * @notice there is no requirement that the deal have expired
+     * @notice all that is required is that the deal is still open, and that there is still a reamining balance
+     * @dev you need to know the index _d of the deal you are trying to close and that is it
+     * @dev only the seller can close this deal
+    */    
     function close(uint _d) external nonReentrant {
         Deal storage deal = deals[_d];
-        require(msg.sender == deal.seller, "not the seller");
-        require(deal.remainingAmount > 0, "all tokens sold");
-        require(deal.open, "already closed");
-        //send back the remainder to the seller
+        require(msg.sender == deal.seller, "HEC04: Only Seller Can Close");
+        require(deal.remainingAmount > 0, "HEC05: All tokens have been sold");
+        require(deal.open, "HEC06: Deal has been closed");
+        /// @dev once we have confirmed it is the seller and there are remaining tokens - physically pull the remaining balances and deliver to the seller
         SafeERC20.safeTransfer(IERC20(deal.token), msg.sender, deal.remainingAmount);
+        /// @dev we now set the remaining amount to 0 and ensure the open flag is set to false, thus this deal can no longer be interacted with 
         deal.remainingAmount = 0;
         deal.open = false;
         emit DealClosed(_d);
     }
 
+    /**
+     * @notice This function is what buyers use to make their OTC purchases
+     * @param _d is the index of the deal that a buyer wants to participate in and make a purchase
+     * @param amount is the amount of tokens the buyer is willing to purchase, which must be at least the minimumPurchase and at most the remainingAmount for this deal
+     * @notice ensure when using this function that you are aware of the minimums, and price per token to ensure sufficient balances to make a purchase
+     * @notice if the deal has an unlockDate that is beyond the current block time - no tokens will be received by the buyer, but rather they will receive
+     * @notice an NFT, which represents their ability to redeem and claim the locked tokens after the unlockDate has passed
+     * @notice the NFT received is a separate smart contract, which also contains the locked tokens
+     * @notice the Seller will receive payment in full immediately when triggering this function, there is no lock on payments
+    */
     function buy(uint _d, uint amount) payable external nonReentrant {
+        /// @dev pull the deal details from storage
         Deal storage deal = deals[_d];
-        require(msg.sender != deal.seller, "youre the seller");
-        require(deal.open && deal.maturity >= block.timestamp, "deal closed");
-        require(msg.sender == deal.buyer || deal.buyer == address(0x0), "not allowed to buy");
-        require((amount >= deal.minimumPurchase || amount == deal.remainingAmount) && deal.remainingAmount >= amount, "not enough");
+        /// @dev we do not let the seller sell to themselves, must be a separate buyer
+        require(msg.sender != deal.seller, "HEC07: Buyer cannot be seller");
+        /// @dev require that the deal order is still valid by checking the open bool, as well as the maturity of the deal being in the future block time
+        require(deal.open && deal.maturity >= block.timestamp, "HEC06: Deal has been closed");
+        /// @dev if the deal had a whitelist - then require the msg.sender to be that buyer, otherwise if there was no whitelist, anyone can buy
+        require(msg.sender == deal.buyer || deal.buyer == address(0x0), "HEC08: Whitelist or buyer allowance error");
+        /// @dev require that the amount being purchased is greater than the deal minimum, or that the amount being purchased is the entire remainder of whats left
+        /// @dev AND require that the remaining amount in the deal actually equals or exceeds what the buyer wants to purchase
+        require((amount >= deal.minimumPurchase || amount == deal.remainingAmount) && deal.remainingAmount >= amount, "HEC09: Insufficient Purchase Size");
+        /// @dev we calculate the purchase amount taking the decimals from the token first
+        /// @dev then multiply the amount by the per token price, and now to get back to an amount denominated in the payment currency divide by the factor of token decimals 
         uint decimals = Decimals(deal.token).decimals();
         uint purchase = (amount * deal.price) / (10 ** decimals);
+        /// @dev check to ensure the buyer actually has enough money to make the purchase
         uint balanceCheck = IERC20(deal.paymentCurrency).balanceOf(msg.sender);
-        require(balanceCheck >= purchase, "not enough to purchase");
+        require(balanceCheck >= purchase, "HECB: Insufficient Balance");
+        /// @dev transfer the purchase to the deal seller
         transferPymt(deal.paymentCurrency, msg.sender, deal.seller, purchase);
         if (deal.unlockDate > block.timestamp) {
-            //creates a futures contract
+            /// @dev if the unlockdate is the in future, then we call our internal function lockTokens to lock those in the NFT contract
             lockTokens(payable(msg.sender), deal.token, amount, deal.unlockDate);
         } else {
+            /// @dev if the unlockDate is in the past or now - then tokens are already unlocked and delivered directly to the buyer
             withdraw(deal.token, payable(msg.sender), amount);
         }
-        
+        /// @dev reduce the deal remaining amount by how much was purchased. If the remainder is 0, then we consider this deal closed and set our open bool to false
         deal.remainingAmount -= amount;
         if (deal.remainingAmount == 0) deal.open = false;
         emit TokensBought(_d, amount, deal.remainingAmount);
     }
 
-    function lockTokens(address payable _owner, address _token, uint _amount, uint _unlockDate) internal {
-        require(_unlockDate > block.timestamp, "no need to lock up");
+    /// @dev internal function that handles the locking of the tokens in the NFT Futures contract
+    /// @dev because this OTC contract is the owner of the NFT contract - it is able to call the function that safely mints an NFT
+    /// @param _owner address here becomes the owner of the NFT
+    /// @param _token address here is the asset that is locked in the NFT Future
+    /// @param _amount is the amount of tokens that will be locked
+    /// @param _unlockDate provides the unlock date which is the expiration date for the Future generated
+    function lockTokens(address _owner, address _token, uint _amount, uint _unlockDate) internal {
+        require(_unlockDate > block.timestamp, "HEC10: Unlocked");
+        /// @dev similar to checking the balances for the OTC contract when creating a new deal - we check the current and post balance in the NFT contract
+        /// @dev to ensure that 100% of the amount of tokens to be locked are in fact locked in the contract address
         uint currentBalance = IERC20(_token).balanceOf(futureContract);
-        //physically creates the future and mints an NFT
+        /// @dev this function points to the NFT Futures contract and calls its function to mint an NFT and generate the locked tokens future struct
         IFuturesNFT(futureContract).createFuture(_owner, _amount, _token, _unlockDate);
-        //now we have to physically send the tokens to the NFT contract as escrow
+        /// @dev physically deliver the amount of tokens to the NFT contract address
         SafeERC20.safeTransfer(IERC20(_token), futureContract, _amount);
+        /// @dev check to make sure that what is received by the futures contract equals the total amount we have delivered
+        /// @dev this prevents functionality with deflationary or tax tokens that have not whitelisted these address 
         uint postBalance = IERC20(_token).balanceOf(futureContract);
-        //check to make sure the entire balance of the funds have arrived in escrow at the NFT contract
         assert(postBalance - currentBalance == _amount);
         emit FutureCreated(_owner, _token, _unlockDate, _amount);
 
     }
 
     
-
+    /// @dev events for each function
     event NewDeal(uint _d, address _seller, address _token, address _paymentCurrency, uint _remainingAmount, uint _minimumPurchase, uint _price, uint _maturity, uint _unlockDate, bool open, address _buyer);
     event TokensBought(uint _d, uint _amount, uint _remainingAmount);
     event DealClosed(uint _d);
